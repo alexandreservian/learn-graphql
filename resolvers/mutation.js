b24c0bad6159d32143c5ab1dffa7186aecf6aefc
alexandreservian <alexandreservian@gmail.com>
2020-04-07 19:33:07 -0300

    creating crud of application

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -3,6 +3,11 @@ const { usuarios} = require('../data/db');
 
 module.exports = {
     novoUsuario(_,{nome, email, idade}){
+        const emailExiste = usuarios.some(({email:emailUser}) => emailUser===email)
+        if(emailExiste){
+            throw new Error("Email já foi cadastro")
+        }
+
         const novo ={
             id: v4(),
             nome,
@@ -13,5 +18,11 @@ module.exports = {
         }
         usuarios.push(novo)
         return novo;
+    },
+    excluirUsuario(_, {id}){
+        const indexDeleted = usuarios.findIndex(({id: idUser}) => id===idUser);
+        if(indexDeleted < 0) return null;
+        const excluidos = usuarios.splice(indexDeleted, 1);
+        return excluidos ? excluidos[0] : null
     }
 }
