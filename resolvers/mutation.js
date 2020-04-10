c5630858e7d6afdeb7997c98306d14bf76e9951c
alexandreservian <alexandreservian@gmail.com>
2020-04-09 23:21:21 -0300

    feat(graphql): adding input to add and delete user

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -1,8 +1,20 @@
 const { v4 } = require('uuid');
 const { usuarios} = require('../data/db');
 
+const indiceUsuario = (filtro) => {
+    if(!filtro) return -1
+    const {id,email} = filtro;
+    if(id){
+        return usuarios.findIndex(u => u.id === id)
+    }else if(email) {
+        return usuarios.findIndex(u => u.email === email)
+    }
+    return -1
+}
+
 module.exports = {
-    novoUsuario(_,{nome, email, idade}){
+    novoUsuario(_,{dados}){
+        const {nome, email, idade} = dados;
         const emailExiste = usuarios.some(({email:emailUser}) => emailUser===email)
         if(emailExiste){
             throw new Error("Email já foi cadastro")
@@ -19,10 +31,20 @@ module.exports = {
         usuarios.push(novo)
         return novo;
     },
-    excluirUsuario(_, {id}){
-        const indexDeleted = usuarios.findIndex(({id: idUser}) => id===idUser);
+    excluirUsuario(_, {filtro}){
+        const indexDeleted = indiceUsuario(filtro);
         if(indexDeleted < 0) return null;
         const excluidos = usuarios.splice(indexDeleted, 1);
         return excluidos ? excluidos[0] : null
+    },
+    alterarUsuario(_, args){
+        const indexDeleted = usuarios.findIndex(({id: idUser}) => args.id===idUser);
+        if(indexDeleted < 0) return null;
+        const usuario = {
+            ...usuarios[indexDeleted],
+            ...args
+        }
+        usuarios.splice(indexDeleted, 1, usuario);
+        return usuario;
     }
 }
